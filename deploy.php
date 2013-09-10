4a783632c627d0f211bbd0a0500201434912ca31
epifrin <panas.yu@gmail.com>
2013-09-10 17:37:46 +0300

    lots updates
    multiple download/upload files
    and other

--- deploy.php
+++ deploy.php
@@ -3,10 +3,34 @@
 * Script for deploying web-site by ftp
 * 
 */
-
-$ini_file = 'deploy_test.ini'; // site or project ini file
 ini_set("max_execution_time","120");
 
+$main_ini_file = 'deploy.ini'; // site or project ini file
+
+if(!is_file($main_ini_file)) exit('Ini-file <b>'.$main_ini_file.'</b> not found');
+$arr_main_ini = parse_ini_file($main_ini_file, true);
+if(!empty($arr_main_ini['sites'])){
+    $arr_ini_list = array();
+    foreach($arr_main_ini['sites'] AS $site_name => $ini_f) if(is_file($ini_f)) $arr_ini_list[$site_name] = $ini_f;
+    if(count($arr_ini_list) > 0) $ini_file = reset($arr_ini_list);
+}
+
+if(!empty($_GET['ini'])){
+    if(!empty($arr_ini_list[$_GET['ini']])){ 
+        $ini_file = $arr_ini_list[$_GET['ini']];
+        setcookie('ini_name', $_GET['ini'], time()+2592000, '/');
+        header('Location: deploy.php');
+        exit();
+    }
+}elseif(!empty($_COOKIE['ini_name'])){
+    if(!empty($arr_ini_list[$_COOKIE['ini_name']])){  
+        $ini_file = $arr_ini_list[$_COOKIE['ini_name']];
+    }else{
+        setcookie('ini_name', '', time()-1, '/');
+    }
+}
+
+if(!is_file($ini_file)) exit('Ini-file <b>'.$ini_file.'</b> not found');
 $arr_ini = parse_ini_file($ini_file, true);
 
 if(!$arr_ini){
@@ -18,9 +42,49 @@ if(!$arr_ini){
 $conn_id = false;
 ftp_conn($arr_ini);
 
-if(!empty($_GET['download'])){
-    if(is_file($arr_ini['deployment']['local_site_dir'].$_GET['download'])){
-        ftp_get($conn_id,$arr_ini['deployment']['local_site_dir'].$_GET['download'],$arr_ini['ftp']['ftp_remote_dir'].str_replace('\\','/',$_GET['download']),FTP_BINARY);
+
+
+
+if($conn_id){
+    // multiple download or upload
+    if(!empty($_POST['atype'])){
+        if(is_array($_POST['files']) && count($_POST['files']) > 0){
+            foreach($_POST['files'] AS $file){
+                if(is_file($arr_ini['deployment']['local_site_dir'].$file)){
+                    if($_POST['atype'] == 'download'){
+                        ftp_get($conn_id,$arr_ini['deployment']['local_site_dir'].$file, $arr_ini['ftp']['ftp_remote_dir'].str_replace('\\','/',$file), FTP_BINARY);
+                    }elseif($_POST['atype'] == 'upload'){
+                        ftp_put($conn_id, $arr_ini['ftp']['ftp_remote_dir'].str_replace('\\','/',$file), $arr_ini['deployment']['local_site_dir'].$file, FTP_BINARY);
+                    }
+                }
+            }
+            header('Location: deploy.php');
+            exit();
+        }
+    }
+    
+    // single download
+    if(!empty($_GET['download'])){
+        if(is_file($arr_ini['deployment']['local_site_dir'].$_GET['download'])){
+            if( ftp_get($conn_id,$arr_ini['deployment']['local_site_dir'].$_GET['download'],$arr_ini['ftp']['ftp_remote_dir'].str_replace('\\','/',$_GET['download']),FTP_BINARY)){
+                header('Location: deploy.php');
+                exit(); 
+            }else{
+                echo 'Failed to download the file '.$_GET['download'];
+            }
+        }
+    }
+
+    // single upload
+    if(!empty($_GET['upload'])){
+        if(is_file($arr_ini['deployment']['local_site_dir'].$_GET['upload'])){
+            if( ftp_put($conn_id, $arr_ini['ftp']['ftp_remote_dir'].str_replace('\\','/',$_GET['upload']), $arr_ini['deployment']['local_site_dir'].$_GET['upload'], FTP_BINARY) ){
+                header('Location: deploy.php');
+                exit(); 
+            }else{
+                echo 'Failed to upload the file '.$_GET['upload'];
+            }
+        }
     }
 }
 
@@ -46,33 +110,83 @@ if($conn_id){
 if($conn_id) ftp_close($conn_id);
 
 ?>
+<html>
+<head>
+</head>
+<body>
+<style>
+    body { 
+        color: #333;
+    }
+    .t_filelist {
+        border-collapse: collapse;
+        border: 1px solid #CCC;
+    }
+    .t_filelist th {
+        border: 1px solid #CCC;
+        color:#555;
+    }
+    .t_filelist td {
+        border: 1px solid #CCC;
+    }
+    .light_gray {
+        background-color:#EFEFEF;
+    }
+    .red {color:red;}
+</style>
 <table>
 <tr>
-    <td>ini=<?=$ini_file?></td>
+    <td>ini=<?=$ini_file?>
+    <form action="" name="form_change_ini">
+        <select name="ini" onchange="document.form_change_ini.submit();">
+        <?php foreach($arr_ini_list AS $site_name=>$ini_f){ ?>
+            <option value="<?=$site_name?>" <?php if($ini_f == $ini_file){ ?>selected="selected"<?php } ?>><?=$site_name?></option>
+        <?php } ?>
+        </select>
+    </form>
+    </td>
 </tr>
 <tr>
     <td>
-    <table border="1" style="border-collapse: collapse;">
+    <form action="" name="form_file_list" method="post">
+    <input type="hidden" name="atype" value="download">
+    <table border="1" cellpadding="2" cellspacing="2" class="t_filelist">
     <tr>
+        <th colspan="4">Local computer</th>
+        <th></th>
+        <th colspan="2" class="light_gray">FTP Server</th>
+        <th colspan="2">Actions</th>
+    </tr>
+    <tr>
+        <th></th>
         <th>File</th>
         <th>Date</th>
-        <th>Size</th>
+        <th>Size, byte</th>
         <th></th>
-        <th>ftp Size</th>
-        <th>ftp Date</th>
+        <th class="light_gray">Size, byte</th>
+        <th class="light_gray">Date</th>
+        <th>From FTP</th>
+        <th>To FTP</th>
     </tr>
     <?php 
     if(!empty($arr_local_files)){
         foreach($arr_local_files AS $file=>$arr_f){ ?>
         <tr>
+            <td><input type="checkbox" name="files[]" value="<?=$file;?>" <?php if(!$arr_f['equal']){ ?> checked="checked" <?php } ?> ></td>
             <td><?=$file;?></td>
             <td><?=date('H:i:s d.m.Y', $arr_f['fdate']);?></td>
             <td align="right"><?=$arr_f['fsize'];?></td>
-            <td align="center"><?php if($arr_f['equal']){ echo '=';}else{echo '!=';}  ?></td>
-            <td align="right"><?=$arr_f['ftp_fsize'];?></td>
-            <td><?php if(!empty($arr_f['ftp_fdate'])){ echo date('H:i:s d.m.Y', $arr_f['ftp_fdate']); }?></td>
-            <td><?php if(!$arr_f['equal'] && $arr_f['ftp_fsize'] > 0){ echo '<input type="button" value="Download from ftp" onclick="window.location.href=\'?download='.$file.'\'">';} ?></td>
-            <td><?php if(!$arr_f['equal']){ echo '<input type="button" value="Upload to ftp">';} ?></td>
+            <td align="center"><?php if($arr_f['equal']){ echo '=';}else{echo '<span class="red">!=</span>';}  ?></td>
+            <td align="right" class="light_gray"><?=$arr_f['ftp_fsize'];?></td>
+            <td class="light_gray"><?php if(!empty($arr_f['ftp_fdate'])){ echo date('H:i:s d.m.Y', $arr_f['ftp_fdate']); }?></td>
+            <td><?php if(!$arr_f['equal'] && $arr_f['ftp_fsize'] > 0){ ?> 
+                <input type="button" value="Download" onclick="window.location.href='?download=<?=urlencode($file);?>'"> 
+                <?php } ?>
+            </td>
+            <td><?php if(!$arr_f['equal'] && $conn_id){ ?> 
+                <input type="button" value="Upload" onclick="window.location.href='?upload=<?=urlencode($file);?>'"> 
+                <?php } ?>
+            </td>
         </tr>
     <?php }
     }else{ ?>
@@ -82,10 +196,19 @@ if($conn_id) ftp_close($conn_id);
     <?php } 
     ?>
     </table>
-    
+    <p><input type="button" value="Download all checked" onclick="btn_click('download');"> <input type="button" value="Upload all checked" onclick="btn_click('upload');"></p>
+    </form>
     </td>
 </tr>
 </table>
+<script>
+function btn_click(atype){
+    document.form_file_list['atype'].value = atype;
+    document.form_file_list.submit();
+}
+</script>
+</body>
+</html>
 <?php
 
 /**
@@ -95,38 +218,30 @@ if($conn_id) ftp_close($conn_id);
 */
 function get_arr_local_files($arr_ini){
     $arr_local_files = array();
+    // include files
     $arr_inc_files = explode("\n", $arr_ini['deployment']['include_files']);
     $local_chdir = $arr_ini['deployment']['local_site_dir'];
     foreach($arr_inc_files AS $str){
         if(!empty($str)){
             if(strpos($str,'*') !== false){
                 if($str == '*'){
-                    $arr_flist = scandir($local_chdir);
-                    if($arr_flist){
-                        foreach($arr_flist AS $str2){
-                            add_file_to_arr(&$arr_local_files, $local_chdir, $str2);
-                        }
-                    }
+                    get_files_tree(&$arr_local_files, '');
+                    break;
                 }else{
-                    if(preg_match('|(.*)\*|s',$str,$res)){
-                        if(is_dir($local_chdir.$res[1])){
-                            $dir = $res[1];
-                            $arr_flist = scandir($local_chdir.$dir);
-                            if($arr_flist){
-                                foreach($arr_flist AS $str2){
-                                    add_file_to_arr(&$arr_local_files, $local_chdir, $dir.$str2);
-                                }
-                            }
+                    if(preg_match("|^(.+)\\*$|s", $str, $res)){
+                        if(is_dir($local_chdir.$res[1])){  
+                            get_files_tree(&$arr_local_files, $res[1]);
                         }
                     }
                 }
                 
-            }else{
-                add_file_to_arr(&$arr_local_files, $local_chdir, $str);
+            }elseif(is_file($local_chdir.$str)){
+                add_file_to_arr(&$arr_local_files, '', $str);
             }
         }
     }
 
+    // exclude files
     $arr_exc_files = explode("\n", $arr_ini['deployment']['exclude_files']);
     foreach($arr_exc_files AS $exc_file){
         if(!empty($exc_file)){
@@ -139,16 +254,45 @@ function get_arr_local_files($arr_ini){
                         unset($arr_local_files[$file]);
                     }
                 }
+            }elseif(substr($exc_file, -2) == DIRECTORY_SEPARATOR.'*'){
+                $ext_folder = substr($exc_file, 0, -1);
+                $ext_len = strlen($ext_folder);
+                foreach($arr_local_files AS $file=>$val){
+                    if(substr($file,0,$ext_len) == $ext_folder){
+                        unset($arr_local_files[$file]);
+                    }
+                }
             } 
         }
     }
     return $arr_local_files;
 }
 
-function add_file_to_arr($arr_local_files, $dir, $file){
-    if(is_file($dir.$file)){
-        $arr_local_files[$file] = array('fsize' => filesize($dir.$file), 'fdate' => filemtime($dir.$file), 'ftp_fsize'=>'', 'ftp_fdate'=>'', 'equal'=>false); 
+function get_files_tree($arr_local_files, $dir){
+    global $arr_ini;
+    $local_chdir = $arr_ini['deployment']['local_site_dir'];  
+    if(is_dir($local_chdir.$dir)){    
+        $arr_list = scandir($local_chdir.$dir);
+        foreach($arr_list AS $val){    
+            if(is_file($local_chdir.$dir.$val)){
+                add_file_to_arr(&$arr_local_files, $dir, $val);
+            }
+            elseif($val != '.' && $val != '..' && is_dir($local_chdir.$dir.$val)){  
+                
+                get_files_tree(&$arr_local_files, $dir.$val.DIRECTORY_SEPARATOR);
+            }
+        }
     }
+    return true;
+}
+
+function add_file_to_arr($arr_local_files, $dir, $file){
+    global $arr_ini;
+    $arr_local_files[$dir.$file] = array('fsize' => filesize($arr_ini['deployment']['local_site_dir'].$dir.$file), 
+            'fdate' => filemtime($arr_ini['deployment']['local_site_dir'].$dir.$file), 
+            'ftp_fsize'=>'', 
+            'ftp_fdate'=>'', 
+            'equal'=>false); 
 }
 
 /**
@@ -177,7 +321,7 @@ function ftp_conn($arr_ini){
 * @param mixed $input
 */
 function p($input){
-    if(is_array($input)){
+    if(is_array($input) || is_object($input)){
         echo '<pre>';
         print_r($input);
         echo '</pre>';
